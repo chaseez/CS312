dffd15ca96c8824d86fae068d8d898f688e0efe3
chasezundel <chasezundel>
2023-10-25 10:31:20 -0600

    Fully functioning min heap

--- project3-network-routing/MinHeap.py
+++ project3-network-routing/MinHeap.py
@@ -1,17 +1,21 @@
 class MinHeap():
     def __init__(self, items):
-        # Stores all the items in tuples (previous, weight)
-        # item[0] = previous node
-        # item[1] = weight
-        self.items = []
-        self.make_queue(items)
         self.PREVIOUS = 0
         self.WEIGHT = 1
         self.LEFT = True
         self.RIGHT = False
 
+        # Stores all the items in tuples (previous, weight)
+        # item[0] = previous node
+        # item[1] = weight
+        self.items = []
+        self.make_queue(items)
+
+
     def make_queue(self, items):
-        self.items = items
+        for item in items:
+            self.insert(item)
+
 
     def get_parent_index(self, pos):
         if pos == 0:
@@ -21,16 +25,29 @@ class MinHeap():
         else:
             return (pos - 1) // 2
 
+
     def get_child_index(self, pos, left):
+        index = None
         if left:
-            return (pos * 2) + 1
+            index = (pos * 2) + 1
         else:
-            return (pos * 2) + 2
+            index = (pos * 2) + 2
+
+        if 0 <= index <= len(self.items) - 1:
+            return index
+        return None
+
+
+    def insert(self, item):
+        self.items.append(item)
+        self.bubble_up(item)
+
 
     def swap_items(self, index_1, index_2):
         temp = self.items[index_2]
         self.items[index_2] = self.items[index_1]
         self.items[index_1] = temp
+
 
     def bubble_up(self, item):
         # Getting the index of the current item
@@ -60,25 +77,64 @@ class MinHeap():
         item_weight = item[self.WEIGHT]
 
         left_child_index = self.get_child_index(item_index, self.LEFT)
-        left_child = self.items[left_child_index]
-        left_weight = left_child[self.WEIGHT]
+        if left_child_index is not None:
+            left_child = self.items[left_child_index]
+            left_weight = left_child[self.WEIGHT]
 
         right_child_index = self.get_child_index(item_index, self.RIGHT)
-        right_child = self.items[right_child_index]
-        right_weight = right_child[self.WEIGHT]
+        if right_child_index is not None:
+            right_child = self.items[right_child_index]
+            right_weight = right_child[self.WEIGHT]
 
-        while item_weight > left_weight and item_weight > right_weight:
-            if left_weight > right_weight:
-                if item_weight > left_weight:
-                    self.swap_items(item_index, left_child_index)
 
-                    item_index = left_child_index
-                    left_child_index = self.get_child_index(item_index, self.LEFT)
+        # If there are more than 2 items in the heap
+        if right_child_index is not None and left_child_index is not None:
+            while item_weight > left_weight or item_weight > right_weight:
+                # Always choose the smallest child to swap with
+                if left_weight < right_weight:
+                    if left_child_index is not None and item_weight > left_weight:
+                        self.swap_items(item_index, left_child_index)
+                        item_index = left_child_index
+
+                else:
+                    if right_child_index is not None and item_weight > right_weight:
+                        self.swap_items(item_index, right_child_index)
+                        item_index = right_child_index
+
+                left_child_index = self.get_child_index(item_index, self.LEFT)
+                if left_child_index is not None:
                     left_child = self.items[left_child_index]
                     left_weight = left_child[self.WEIGHT]
 
-                    right_child_index = self.get_child_index(item_index, self.RIGHT)
+                right_child_index = self.get_child_index(item_index, self.RIGHT)
+                if right_child_index is not None:
                     right_child = self.items[right_child_index]
                     right_weight = right_child[self.WEIGHT]
+
+                if right_child_index is None or left_child_index is None:
+                    break
+
+        # If there are 2 items in the heap
+        elif left_child_index is not None:
+            if left_child_index is not None and item_weight > left_weight:
+                self.swap_items(item_index, left_child_index)
+                item_index = left_child_index
+
+
+    def pop(self):
+        if len(self.items) > 1:
+            self.swap_items(-1,0)
+            min = self.items.pop()
+            self.sift_down(self.items[0], 0)
+            return min
+        else: return self.items.pop()
+
+
+    def decrease_key(self, item):
+        for i in range(len(self.items)):
+            if self.items[i][0] == item[0]:
+                self.items[i] = item
+
+        self.bubble_up(item)
 
 
--- project3-network-routing/tester_min_heap.py
+++ project3-network-routing/tester_min_heap.py
@@ -1,18 +1,52 @@
 from MinHeap import *
 from math import inf as INF
 
-items = [('A',0),('B', INF), ('C', INF), ('D', INF), ('E', INF), ('F', INF), ('G', INF)]
+items = [('A',6),('B', 0), ('C', 0), ('D', 4), ('E', 7), ('F', 1), ('G', 0)]
 
 min_heap = MinHeap(items)
 
 print(min_heap.items)
 
-min_heap.swap_items(0, 6)
+print(min_heap.pop())
+print(min_heap.items)
+
+print(min_heap.pop())
+print(min_heap.items)
+
+print(min_heap.pop())
+print(min_heap.items)
+
+print(min_heap.pop())
+print(min_heap.items)
+
+print(min_heap.pop())
+print(min_heap.items)
+
+print(min_heap.pop())
+print(min_heap.items)
+
+print(min_heap.pop())
+print(min_heap.items)
+
+min_heap.insert(('b', 6))
+min_heap.insert(('q', INF))
+min_heap.insert(('d', 3))
+min_heap.insert(('e', INF))
+min_heap.insert(('a', 9))
+min_heap.insert(('c', 4))
+min_heap.insert(('n', INF))
+min_heap.insert(('l', INF))
 
 print(min_heap.items)
 
-min_heap.bubble_up(min_heap.items[6])
+min_heap.decrease_key(('q', 4))
+min_heap.decrease_key(('e', 10))
+min_heap.decrease_key(('n', 15))
+min_heap.decrease_key(('q', 0))
 
 print(min_heap.items)
+
+
+
 
 
