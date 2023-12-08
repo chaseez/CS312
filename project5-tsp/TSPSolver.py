5d6d4db5ae6bed43d44ad5ce558c73f5b19ee316
chasezundel <chasezundel>
2023-12-07 19:41:28 -0700

    Working reduced cost matrix functions and lower bound calculating

--- project5-tsp/TSPSolver.py
+++ project5-tsp/TSPSolver.py
@@ -81,7 +81,42 @@ class TSPSolver:
 	'''
 
 	def greedy( self,time_allowance=60.0 ):
-		pass
+		results = {}
+		cities = self._scenario.getCities()
+		ncities = len(cities)
+		foundTour = False
+		count = 0
+		bssf = None
+		adjacency_matrix = np.array(self._scenario._edge_exists.copy())
+
+		adjacency_cost_matrix = self.generate_adjacency_matrix(adjacency_matrix, cities)
+
+		lower_bound, reduced_cost_matrix = self.compute_lower_bound(adjacency_cost_matrix)
+
+		print(lower_bound, reduced_cost_matrix)
+
+		start_time = time.time()
+		while not foundTour and time.time() - start_time < time_allowance:
+			# create a random permutation
+			for i in range(ncities):
+				route = []
+				# Get a function to find the shortest path based on the adjacency matrix
+				bssf = TSPSolution(route)
+				count += 1
+				if bssf.cost < np.inf:
+					# Found a valid route
+					foundTour = True
+
+		end_time = time.time()
+		results['cost'] = bssf.cost if foundTour else math.inf
+		results['time'] = end_time - start_time
+		results['count'] = count
+		results['soln'] = bssf
+		results['max'] = None
+		results['total'] = None
+		results['pruned'] = None
+		return results
+
 
 
 
@@ -110,3 +145,49 @@ class TSPSolver:
 
 	def fancy( self,time_allowance=60.0 ):
 		pass
+
+	def generate_adjacency_matrix(self, adjacency_matrix, cities):
+		cost_matrix = []
+		for i in range(len(adjacency_matrix)):
+			src = cities[i]
+			cost_matrix.append([])
+			for j in range(len(adjacency_matrix)):
+				target = cities[j]
+				if adjacency_matrix[i,j]:
+					cost_matrix[i].append(src.costTo(target))
+				else:
+					cost_matrix[i].append(math.inf)
+
+		return np.array(cost_matrix)
+
+	def compute_lower_bound(self, adjacency_cost_matrix):
+		reduced_outbound_matrix = []
+		lower_bound = 0
+
+		# Get the least cost for each outbound path
+		for row in adjacency_cost_matrix:
+			least_cost = min(row)
+			lower_bound += least_cost
+
+			# print(f'Before reduction: {row}')
+			row -= least_cost
+			# print(f'After reduction: {row}')
+			reduced_outbound_matrix.append(row)
+
+		reduced_outbound_matrix = np.array(reduced_outbound_matrix)
+
+		reduced_cost_matrix = reduced_outbound_matrix.copy()
+
+		# print(f'Reduced outbound matrix: {reduced_cost_matrix}')
+		for i in range(len(reduced_cost_matrix)):
+			least_cost = min(reduced_cost_matrix[:,i])
+			if least_cost == 0:
+				continue
+			lower_bound += least_cost
+
+			# print(f'Before reduction: {reduced_cost_matrix[:,i]}')
+			reduced_cost_matrix[:,i] -= least_cost
+			# print(f'After reduction: {reduced_cost_matrix[:,i]}')
+
+		return (lower_bound, reduced_cost_matrix)
+
